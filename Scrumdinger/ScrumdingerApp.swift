4337a615ace6337a9c24419f0674ccee72d9b4c0
daiki-skm <daikisakuma6040@gmail.com>
2023-04-08 15:06:33 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 佐久間大空 on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
