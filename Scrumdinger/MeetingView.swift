6b9a073d79de497c5fc3678a62e4012749478a03
daiki-skm <daikisakuma6040@gmail.com>
2023-04-09 11:09:00 +0900

    wip

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 佐久間大空 on 2023/04/08.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
